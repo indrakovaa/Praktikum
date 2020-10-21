f5979958366d795f66c377f9212b78c4ae71fc8d
indrakovaa <indrakova.a@gmail.com>
2020-10-21 15:29:25 +0200

    Update penguins_script.R

--- penguins_script.R
+++ penguins_script.R
@@ -5,18 +5,24 @@ library(readr)
 penguins_size <- read_csv("./Penguins/penguins_size.csv")
 data <- drop_na(penguins_size)
 
-#recode Island and Sex
-levels(as.factor(data$island))
+#Recode island, sex, and species
+levels(as.factor(data$species))
 data$island[data$island == "Biscoe"] <- 1
 data$island[data$island == "Dream"]<-2
 data$island[data$island == "Torgersen"]<-3
 
-data$island[data$island == "MALE"]<-1
-data$island[data$island == "FEMALE"]<-2
+data$sex[data$sex == "MALE"]<-1
+data$sex[data$sex == "FEMALE"]<-2
+
+data$species[data$species == "Adelie"]<-1
+data$species[data$species == "Chinstrap"]<-2
+data$species[data$species == "Gentoo"]<-3
+
+data<-mutate_if(data, is.character, as.double)
 
 # boxplot for visualization of variable culmen depth
 data %>%
-  ggplot(aes(x=island, y=culmen_depth_mm, group=island))+
+ggplot(aes(x=species, y=culmen_depth_mm, group=species))+
   geom_boxplot()
 
 # train and test set
@@ -26,6 +32,20 @@ train_data = data[train_ind, ]
 test_data = data[-train_ind, ]
 
 # normalization
+label_col = "species"  
+feature_cols = c("island", "culmen_length_mm", "culmen_depth_mm", "flipper_length_mm", "body_mass_g" ) 
+## should we include sex in the species prediction??
 
+train_means = apply(train_data[feature_cols], 2, mean)
+train_sds = apply(train_data[feature_cols], 2, sd)
+
+train_features = scale(train_data[feature_cols], 
+                       center = train_means,
+                       scale = train_sds)
+test_features = scale(test_data[feature_cols], 
+                      center = train_means,
+                      scale = train_sds)
+train_labels = train_data[label_col]
+test_labels = test_data[label_col]
 
 # Species prediction
